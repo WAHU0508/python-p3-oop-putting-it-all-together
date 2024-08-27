f8c2ed2a189ccfb67ba5d34bc0356b05fa618463
WAHU0508 <sharongikenye@gmail.com>
2024-08-27 14:09:44 +0300

    Get the shoe.py tests passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    def get_brand(self):
+        return self._brand
+    def set_brand(self, brand):
+        self._brand = brand
+    brand = property(get_brand, set_brand)
+
+    def get_size(self):
+        return self._size
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    size = property(get_size, set_size)
+
+    def cobble(self, condition="Old"):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+stan_smith = Shoe("Adidas", 9)
+print(stan_smith.brand)
+print(stan_smith.size)
+stan_smith.size = "not an integer"
+stan_smith.cobble()
+print(stan_smith.condition)
